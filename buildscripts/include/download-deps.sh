efa9c53d295db184eaca4ea2eeb01e7997dab794
DC87cVDpyMiUBzNY <142811917+DC87cVDpyMiUBzNY@users.noreply.github.com>
2026-09-18 19:50:29 +0900

    add patches

--- buildscripts/include/download-deps.sh
+++ buildscripts/include/download-deps.sh
@@ -22,6 +22,8 @@ if [ ! -d ffmpeg ]; then
 	args=()
 	[ $IN_CI -eq 1 ] && args+=(--depth=1 -b "$v_ci_ffmpeg")
 	git clone https://github.com/FFmpeg/FFmpeg ffmpeg "${args[@]}"
+	patch -d ffmpeg -p1 < ../patches/ffmpeg-hw-profile-check.patch
+	patch -d ffmpeg -p1 < ../patches/avcodec-mediacodecdec_common-export-correct-dimensio.patch
 fi
 
 # freetype2
